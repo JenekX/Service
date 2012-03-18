db29bd2478e4958e8561af52acaef7da52303a4f
Eugene Zenkov <JenekX@Rambler.ru>
2012-03-18 17:02:17 +0400

    First commit

---
+++ Service/Service.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.ComponentModel;
+using System.ServiceModel;
+using System.ServiceProcess;
+using System.Configuration;
+using System.Configuration.Install;
+
+namespace Microsoft.ServiceModel.Samples
+{
+    [ServiceContract(Namespace = "http://Microsoft.ServiceModel.Samples")]
+    public interface ICalculator
+    {
+        [OperationContract]
+        double Add(double n1, double n2);
+        [OperationContract]
+        double Subtract(double n1, double n2);
+        [OperationContract]
+        double Multiply(double n1, double n2);
+        [OperationContract]
+        double Divide(double n1, double n2);
+    }
+
+    public class CalculatorService : ICalculator
+    {
+        // Implement the ICalculator methods.
+        public double Add(double n1, double n2)
+        {
+            double result = n1 + n2;
+            return result;
+        }
+
+        public double Subtract(double n1, double n2)
+        {
+            double result = n1 - n2;
+            return result;
+        }
+
+        public double Multiply(double n1, double n2)
+        {
+            double result = n1 * n2;
+            return result;
+        }
+
+        public double Divide(double n1, double n2)
+        {
+            double result = n1 / n2;
+            return result;
+        }
+    }
+
+    public class CalculatorWindowsService : ServiceBase
+    {
+        public ServiceHost serviceHost = null;
+        public CalculatorWindowsService()
+        {
+            // Name the Windows Service
+            ServiceName = "WCFWindowsServiceSample";
+        }
+
+        public static void Main()
+        {
+            ServiceBase.Run(new CalculatorWindowsService());
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            if (serviceHost != null)
+            {
+                serviceHost.Close();
+            }
+
+            serviceHost = new ServiceHost(typeof(CalculatorService));
+
+            serviceHost.Open();
+        }
+
+        protected override void OnStop()
+        {
+            if (serviceHost != null)
+            {
+                serviceHost.Close();
+                serviceHost = null;
+            }
+        }
+    }
+
+    [RunInstaller(true)]
+    public class ProjectInstaller : Installer
+    {
+        private ServiceProcessInstaller process;
+        private ServiceInstaller service;
+
+        public ProjectInstaller()
+        {
+            process = new ServiceProcessInstaller();
+            process.Account = ServiceAccount.LocalSystem;
+            service = new ServiceInstaller();
+            service.ServiceName = "WCFWindowsServiceSample";
+            Installers.Add(process);
+            Installers.Add(service);
+        }
+    }
+}
---
+++ TestService/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using TestService.ServiceReference1;
+
+namespace TestService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CalculatorClient client = new CalculatorClient();
+
+            Console.WriteLine(client.Add(10, 20));
+            Console.ReadKey();
+
+            client.Close();
+        }
+    }
+}
